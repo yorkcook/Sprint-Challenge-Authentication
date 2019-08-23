d0190483bd9d993093e6322739a7660a50152b6f
York Cook <yorkcook@Johnny-Castle.lan1>
2019-08-23 13:45:33 -0400

    WIP: 2 tests still failing

--- api/server.spec.js
+++ api/server.spec.js
@@ -2,6 +2,8 @@ const request = require("supertest");
 const server = require("../api/server");
 const db = require("../database/dbConfig.js");
 
+let token;
+
 describe("Post /register", () => {
   beforeEach(async () => {
     await db("users").truncate();
@@ -15,6 +17,7 @@ describe("Post /register", () => {
       })
       .then(res => {
         expect(res.status).toBe(201);
+        token;
       });
   });
   it.skip("returns user object", () => {
@@ -45,9 +48,37 @@ describe("Post /login", () => {
         expect(res.status).toBe(200);
       });
   });
-  it("returns user object", () => {
+  it.skip("returns user object", () => {
     return request(server)
       .post("/api/auth/login")
+      .send({
+        username: "Gabe",
+        password: "Ball4theGame"
+      })
+      .then(res => {
+        expect(typeof res.body).toBe("object");
+      });
+  });
+});
+
+describe("Get /jokes", () => {
+  beforeEach(async () => {
+    await db("users").truncate();
+  });
+  it("returns 200", () => {
+    return request(server)
+      .post("/api/login")
+      .send({
+        username: "Gabe",
+        password: "Ball4theGame"
+      })
+      .then(res => {
+        expect(res.status).toBe(200);
+      });
+  });
+  it.skip("returns user object", () => {
+    return request(server)
+      .post("/api/jokes")
       .send({
         username: "Gabe",
         password: "Ball4theGame"
